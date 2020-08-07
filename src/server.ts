37f1494b953929d0c4ac429e138df145de9e380c
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-06 22:54:25 -0300

    refactor: create routes in separated file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  const users = [
-    { name: 'Carlos Lima', age: 25 },
-    { name: 'Felipe', age: 30 },
-    { name: 'Diego', age: 25 }
-  ]
-  return res.json(users)
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(5000)
